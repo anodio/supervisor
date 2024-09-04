a4c2f244f83b3b1743b0f692ddee7750e688de65
Vladimir <vladimir@sobaka.com>
2024-09-04 17:03:41 +0200

    supervisor fixed for metrics

--- src/Control/SupervisorControlCenter.php
+++ src/Control/SupervisorControlCenter.php
@@ -2,9 +2,11 @@
 
 namespace Anodio\Supervisor\Control;
 
+use Anodio\Core\Helpers\Log;
 use Anodio\Supervisor\Configs\SupervisorConfig;
 use Anodio\Supervisor\Interfaces\WorkerLockerInterface;
 use Anodio\Supervisor\WorkerManagement\WorkerManager;
+use Prometheus\Storage\InMemory;
 use Swow\Channel;
 use Swow\Coroutine;
 
@@ -14,20 +16,22 @@ class SupervisorControlCenter
     private ?WorkerLockerInterface $workerLocker; // we dont need em, if we run in worker-dev-mode
     private ?WorkerManager $workerManager; // we dont need em, if we run in worker-dev-mode
     private SupervisorConfig $config;
+    private \Prometheus\Storage\Adapter $metricsStorage;
 
-    public function __construct(Channel $supervisorControlChannel, ?WorkerLockerInterface $workerLocker, ?WorkerManager $workerManager, SupervisorConfig $config)
+    public function __construct(Channel $supervisorControlChannel, ?WorkerLockerInterface $workerLocker, ?WorkerManager $workerManager, SupervisorConfig $config, InMemory $inMemoryMetricsStorage)
     {
         $this->workerLocker = $workerLocker;
         $this->supervisorControlChannel = $supervisorControlChannel;
         $this->workerManager = $workerManager;
         $this->config = $config;
+        $this->metricsStorage = $inMemoryMetricsStorage;
     }
 
     public function control(): void
     {
         while (true) {
             try {
-                $message = $this->supervisorControlChannel->pop(250000);
+                $message = $this->supervisorControlChannel->pop();
             } catch (\Swow\ChannelException $e) {
                 if (str_starts_with($e->getMessage(), 'Channel wait producer failed, reason: Timed out for')) {
                     $message = null;
@@ -37,12 +41,27 @@ class SupervisorControlCenter
             }
             if (!is_null($message)) {
                 $this->handleMessage($message);
+            } else {
+                Log::warning('No message from supervisor control channel');
             }
         }
     }
 
     public function handleMessage(array $message): void
     {
+        if ($message['command'] === 'updateCounterMetrics' && $message['sender'] === 'worker') {
+            $this->metricsStorage->updateCounter($message['data']);
+        }
+        if ($message['command'] === 'updateGaugeMetrics' && $message['sender'] === 'worker') {
+            $this->metricsStorage->updateGauge($message['data']);
+        }
+        if ($message['command'] === 'updateHistogramMetrics' && $message['sender'] === 'worker') {
+            $this->metricsStorage->updateHistogram($message['data']);
+        }
+        if ($message['command'] === 'updateSummaryMetrics' && $message['sender'] === 'worker') {
+            $this->metricsStorage->updateSummary($message['data']);
+        }
+
         if ($message['command'] === 'workerStats' && $message['sender'] === 'worker') {
             $memory = $message['stats']['memory']/1024/1024; //mb
             if ($memory > $this->config->maxMemory) {
